ec7989156ea707e247e66c410d3f292bd16f388e
gabriel gonçalves da silva <gabriel24f90@gmail.com>
2022-05-31 11:43:41 -0300

    api novaaaaa

--- api/src/repository/conection.js
+++ api/src/repository/conection.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise'
 
 const con = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    users: process.env.MYSQL_USER,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PWD,
-    dataBase: process.env.MYSQL_DB
+    database: process.env.MYSQL_DB
 })
 
 export { con }
